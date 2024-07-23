520aa6addd7040668282784dd5a7954d37c4275a
bluemcs <admin@bluemcs.com>
2024-07-23 18:29:04 +0700

    Deploy website - based on 57216f19546c199e11aa68228353c045c12a605a

--- assets/js/36994c47.14674a06.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbluemcs_email=self.webpackChunkbluemcs_email||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- assets/js/acecf23e.168ebeac.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbluemcs_email=self.webpackChunkbluemcs_email||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/bluemcs.email/blog","blogTitle":"Blog"}')}}]);
