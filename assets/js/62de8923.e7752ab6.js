3652487adfb8c7cad02c4b2cb4992ff3084aca64
bluemcs <admin@bluemcs.com>
2024-07-24 10:07:07 +0700

    Deploy website - based on 57216f19546c199e11aa68228353c045c12a605a

---
+++ assets/js/62de8923.e7752ab6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbluemcs_email=self.webpackChunkbluemcs_email||[]).push([[122],{3875:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/bluemcs.email/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
